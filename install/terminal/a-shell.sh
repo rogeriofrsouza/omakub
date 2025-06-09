9d846cd1ffee6f9e99c9fbd11a537a165922b6a1
Rogério Ferreira de Souza <rogeriofrsouza@gmail.com>
2025-06-08 21:59:50 -0300

    Add and source zsh plugins

--- install/terminal/a-shell.sh
+++ install/terminal/a-shell.sh
@@ -7,6 +7,13 @@ sudo apt install -y zsh
 chsh -s $(which zsh)
 cp ~/.local/share/omakub/configs/zshrc ~/.zshrc
 
+# ZSH plugins
+export ZSH_PLUGINS_DIR="~/.zsh"
+git clone https://github.com/Aloxaf/fzf-tab $ZSH_PLUGINS_DIR/fzf-tab
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_PLUGINS_DIR/zsh-autosuggestions
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting $ZSH_PLUGINS_DIR/fast-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-completions.git $ZSH_PLUGINS_DIR/zsh-completions.git
+
 # Load the PATH for use later in the installers
 source ~/.local/share/omakub/defaults/bash/shell
 
